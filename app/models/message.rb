b2e802323d6566b75488daeab2bb31254add6476
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-27 09:01:18 -0800

    Sends text messages

---
+++ app/models/message.rb
@@ -0,0 +1,19 @@
+class Message < ActiveRecord::Base
+  before_create :send_sms
+
+
+  private
+
+  def send_sms
+    response = RestClient::Request.new(
+      :method => :post,
+      :url => 'https://api.twilio.com/2010-04-01/Accounts/AC9a870cbd26c9791c232293387dd40777/Messages.json',
+      :user => ENV['TWILIO_ACCOUNT_SID'],
+      :password => ENV['TWILIO_AUTH_TOKEN'],
+      :payload => {
+        :Body => body,
+        :To => to,
+        :From => from }
+    ).execute
+  end
+end
