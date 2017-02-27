0ebac3da22b0cdac8208192a97a0bec09e093f8c
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-27 09:24:42 -0800

    Can add new contacts

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,30 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:notice] = "Contact added"
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :number)
+  end
+
+end
