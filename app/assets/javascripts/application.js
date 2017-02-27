6313d2dac7d9784aeeca8f9798048ce29e443e9c
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-27 10:38:50 -0800

    Will append new forms for adding numbers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(){
+  $("#addAnotherNumber").click(function(){
+    $("#to").append($("#new_number_form").html());
+  });
+});
--- app/models/message.rb
+++ app/models/message.rb
@@ -5,15 +5,19 @@ class Message < ActiveRecord::Base
   private
 
   def send_sms
-    response = RestClient::Request.new(
-      :method => :post,
-      :url => 'https://api.twilio.com/2010-04-01/Accounts/AC9a870cbd26c9791c232293387dd40777/Messages.json',
-      :user => ENV['TWILIO_ACCOUNT_SID'],
-      :password => ENV['TWILIO_AUTH_TOKEN'],
-      :payload => {
-        :Body => body,
-        :To => to,
-        :From => from }
-    ).execute
+    begin
+      response = RestClient::Request.new(
+        :method => :post,
+        :url => 'https://api.twilio.com/2010-04-01/Accounts/AC9a870cbd26c9791c232293387dd40777/Messages.json',
+        :user => ENV['TWILIO_ACCOUNT_SID'],
+        :password => ENV['TWILIO_AUTH_TOKEN'],
+        :payload => {
+          :Body => body,
+          :To => to,
+          :From => from }
+      ).execute
+    rescue
+      false
+    end
   end
 end
