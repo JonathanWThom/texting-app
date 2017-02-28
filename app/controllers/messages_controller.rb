ce669e9e0cc327b1b069992b0dec914f866d15a1
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-28 08:34:45 -0800

    ngrok

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -38,9 +38,26 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
   end
 
+  def reply
+    message_body = params["Body"]
+    from_number = params["From"]
+    boot_twilio
+    sms = @client.messages.create(
+      from: Rails.application.secrets.twilio_number,
+      to: from_number,
+      body: "Hey, thanks for the text. You number is #{from_number}. In case you didn't know."
+    )
+  end
+
 private
 
   def message_params
     params.require(:message).permit(:to, :from, :body)
+  end
+
+  def boot_twilio
+    account_sid = Rails.application.secrets.twilio_sid
+    auth_token = Rails.application.secrets.twilio_token
+    @client = Twilio::REST::CLIENT.new account_sid, auth_token
   end
 end
